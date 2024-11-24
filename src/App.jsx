6c73940dd22cd5b4473826ba8024e56a486752c6
AC <chenyibo416@gmail.com>
2024-11-24 10:46:03 +0800

    fix sidebar and course page main css attri for responsive size

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import "./App.css";
+import CoursePage from "../src/pages/CoursePage";
+
+function App() {
+  return (
+    <>
+      <CoursePage />
+    </>
+  );
+}
+
+export default App;
